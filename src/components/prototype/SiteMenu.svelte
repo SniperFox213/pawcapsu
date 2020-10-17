728bd5b046fcd9f6b42eba1faf4449dfc4f91895
SniperFox213 <dj.gleb@list.ru>
2020-10-17 09:50:28 +0300

    Landing Changes

---
+++ src/components/prototype/SiteMenu.svelte
@@ -0,0 +1,88 @@
+<script>
+  import Popover from "svelte-popover";
+
+  let siteMenuOpened = false;
+
+  let button02Hovered = false;
+  let button03Hovered = false;
+</script>
+
+<Popover placement="bottom-start" arrow={false} bind:open={siteMenuOpened} overlayColor="rgba(0,0,0,0.25)">
+  <div slot="target" class="flex items-center">
+    <button on:mouseover={(e) => button02Hovered = true} on:mouseout={(e) => button02Hovered = false} class="px-2 py-2 rounded-md { siteMenuOpened || button02Hovered ? "bg-white" : "" }">
+      <svg style="height: 1.4rem; width: 1.4rem;" xmlns="http://www.w3.org/2000/svg" fill="{ button02Hovered || siteMenuOpened ? "#000" : "#fff" }" viewBox="0 0 173.24 181.66"><defs></defs><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><path class="cls-1" d="M140.38,119.5,112.45,91.57a37.23,37.23,0,0,0-52.16,0L32.86,119a36.42,36.42,0,0,0,0,51.5h0a36.42,36.42,0,0,0,51.5,0l2-2L88.88,171a36.42,36.42,0,0,0,51.5,0h0A36.42,36.42,0,0,0,140.38,119.5Z"/><ellipse class="cls-1" cx="22.2" cy="71.96" rx="21.36" ry="31.57" transform="matrix(0.97, -0.26, 0.26, 0.97, -17.87, 8.2)"/><ellipse class="cls-1" cx="151.04" cy="71.96" rx="31.57" ry="21.36" transform="translate(42.44 199.23) rotate(-75)"/><ellipse class="cls-1" cx="63.26" cy="31.57" rx="21.36" ry="31.57"/><ellipse class="cls-1" cx="112.67" cy="31.57" rx="21.36" ry="31.57"/></g></g></svg>
+    </button>
+
+    {#if siteMenuOpened}
+      <div class="relative ml-1 py-1 px-3 pr-12">
+        <h1 class="text-xl text-white font-semibold">Paws</h1>
+
+        <span class="absolute top-0 right-0 rounded-full bg-red-500 px-2 py-1 text-extra-xss text-white">
+          prototype
+        </span>
+      </div>
+    {/if}
+  </div>
+
+  <div slot="content" class="pt-4 pr-8 md:pr-0 w-full md:w-auto min-w-screen md:min-w-26rem">
+    <div class="bg-white relative rounded-md shadow-lg px-3 py-6">
+      <!-- Navbar -->
+      <div class="w-full py-3 text-xs flex justify-center items-center">
+        <button class="mx-2 text-sm border-b-1 border-dotted border-gray-100 text-black">
+          Контент
+        </button>
+
+        <button class="mx-2 text-gray-700">
+          Общение
+        </button>
+
+        <button class="mx-2 text-gray-700">
+          Помощь
+        </button>
+      </div>
+
+      <!-- 
+        Content 
+        @type: Content
+        -->
+      <div class="flex flex-wrap justify-center">
+        <div class="w-32 h-32 px-3 py-3 relative">
+          <div on:click={(e) => {}} on:mouseover={(e) => { button03Hovered = true }} on:mouseout={(e) => { button03Hovered = false }} style="{ button03Hovered ? "background-color: white;" : "background: linear-gradient(to right, #0f2027, #203a43, #2c5364);" }" class="w-full h-full cursor-pointer flex flex-col items-center justify-center rounded-md bg-gray-200 shadow-md">
+            <!-- Image -->
+            <svg style="width: 1.6rem; height: 1.6rem;" class="mb-3" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="{ button03Hovered ? "#000" : "#fff" }" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path><path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path></svg>
+
+            <!-- Some Texts -->
+            <h1 class="text-sm { button03Hovered ? "text-black" : "text-white" }">Рассказы</h1>
+          </div>
+        </div>
+
+        <div class="w-32 h-32 px-3 py-3 relative">
+          <div on:click={(e) => {}} style="background: linear-gradient(to right, #00b4db, #0083b0);" class="w-full h-full cursor-pointer flex flex-col items-center justify-center rounded-md bg-gray-200 shadow-md">
+            <!-- Image -->
+            <svg style="width: 1.6rem; height: 1.6rem;" class="mb-3" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect><circle cx="8.5" cy="8.5" r="1.5"></circle><polyline points="21 15 16 10 5 21"></polyline></svg>
+
+            <!-- Some Texts -->
+            <h1 class="text-sm text-white">Картинки</h1>
+          </div>
+        </div>
+
+        <div class="w-32 h-32 px-3 py-3 relative">
+          <div on:click={(e) => {}} style="background: linear-gradient(to right, #11998e, #38ef7d);" class="w-full h-full cursor-pointer flex flex-col items-center justify-center rounded-md bg-gray-200 shadow-md">
+            <!-- Image -->
+            <svg style="width: 1.6rem; height: 1.6rem;" class="mb-3" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="22 12 18 12 15 21 9 3 6 12 2 12"></polyline></svg>
+
+            <!-- Some Texts -->
+            <h1 class="text-sm text-white">Новости</h1>
+          </div>
+        </div>
+      </div>
+
+      <!-- "Show all" Button -->
+      <div class="mt-3 w-full flex justify-center">
+        <button class="text-xs text-gray-700 border-b-1 border-dotted border-gray-700">
+          Показать всё
+        </button>
+      </div>
+    </div>
+  </div>
+</Popover>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -11,7 +11,10 @@
 	import { onMount } from "svelte";
 
 	// Importing components
-	import Popover from "svelte-popover";
+
+	// Importing PROTOTYPE components
+	import SiteMenu from "../components/prototype/SiteMenu.svelte";
+	import UserLine from "../components/prototype/UserlineHeader.svelte";
 
 	// Let's now change our loaded state on component mount.
 	onMount(() => {
@@ -20,10 +23,7 @@
 		}, 25)
 	});
 
-	let siteMenuOpened = false;
-
-	let button01Hovered = false;
-	let button02Hovered = false;
+	let posts = [1,2,3,4,5,6,7,8];
 </script>
 
 <style>
@@ -35,90 +35,88 @@
 		<!-- Header -->
 		<header style="z-index: 1;" class="bg-black fixed w-full px-4 py-2 md:px-12 flex justify-between items-center">
 			<!-- Logotype -->
-			<Popover placement="bottom-start" arrow={false} bind:open={siteMenuOpened} overlayColor="rgba(0,0,0,0.25)">
-				<button slot="target" on:mouseover={(e) => button02Hovered = true} on:mouseout={(e) => button02Hovered = false} class="px-2 py-2 rounded-md { siteMenuOpened || button02Hovered ? "bg-white" : "" }">
-					<svg style="height: 1.4rem; width: 1.4rem;" xmlns="http://www.w3.org/2000/svg" fill="{ button02Hovered || siteMenuOpened ? "#000" : "#fff" }" viewBox="0 0 173.24 181.66"><defs></defs><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><path class="cls-1" d="M140.38,119.5,112.45,91.57a37.23,37.23,0,0,0-52.16,0L32.86,119a36.42,36.42,0,0,0,0,51.5h0a36.42,36.42,0,0,0,51.5,0l2-2L88.88,171a36.42,36.42,0,0,0,51.5,0h0A36.42,36.42,0,0,0,140.38,119.5Z"/><ellipse class="cls-1" cx="22.2" cy="71.96" rx="21.36" ry="31.57" transform="matrix(0.97, -0.26, 0.26, 0.97, -17.87, 8.2)"/><ellipse class="cls-1" cx="151.04" cy="71.96" rx="31.57" ry="21.36" transform="translate(42.44 199.23) rotate(-75)"/><ellipse class="cls-1" cx="63.26" cy="31.57" rx="21.36" ry="31.57"/><ellipse class="cls-1" cx="112.67" cy="31.57" rx="21.36" ry="31.57"/></g></g></svg>
-				</button>
+			<SiteMenu />
 
-				<div slot="content" class="pt-4 pr-8 md:pr-0 w-full md:w-auto min-w-screen md:min-w-26rem">
-					<div class="bg-white relative rounded-md shadow-lg px-3 py-6">
-						<!-- Navbar -->
-						<div class="w-full py-3 text-xs flex justify-center items-center">
-							<button class="mx-2 text-sm border-b-1 border-dotted border-gray-100 text-black">
-								Контент
-							</button>
+			<!-- Page-related content -->
+			<div class="w-full flex items-center md:px-8">
+				<!-- Search-bar -->
+				<div class="rounded-full flex items-center bg-white px-4 py-1 w-1/2">
+					<!-- Search Icon -->
+					<svg style="width: 1rem; height: 1rem;" class="mr-2" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
 
-							<button class="mx-2 text-gray-700">
-								Общение
-							</button>
-
-							<button class="mx-2 text-gray-700">
-								Помощь
-							</button>
-						</div>
-
-						<!-- Content -->
-						<div class="flex flex-wrap justify-center">
-							<div class="w-32 h-32 px-3 py-3 relative">
-								<div class="w-full h-full rounded-md bg-red-500">
-								
-								</div>
-							</div>
-
-							<div class="w-32 h-32 px-3 py-3 relative">
-								<div class="w-full h-full rounded-md bg-red-500">
-								
-								</div>
-							</div>
-
-							<div class="w-32 h-32 px-3 py-3 relative">
-								<div class="w-full h-full rounded-md bg-red-500">
-								
-								</div>
-							</div>
-						</div>
-
-						<!-- "Show all" Button -->
-						<div class="mt-3 w-full flex justify-center">
-							<button class="text-xs text-gray-700 border-b-1 border-dotted border-gray-700">
-								Показать всё
-							</button>
-						</div>
-					</div>
+					<input class="w-full text-black" type="text" placeholder="Поиск...">
 				</div>
-			</Popover>
+
+				<!-- Other Things -->
+				<!-- <div class="w-full relative px-4">
+					#Overlay
+					<div style="z-index: 2; background: linear-gradient(90deg, rgba(255,255,255,0) 0%, rgba(0,0,0,1) 100%);" class="absolute inset-x-0 w-full h-full"></div>
+
+					<div style="z-index: 1;" class="flex">
+						<p class="text-gray-100 text-sm">#Lorem ipsum</p>
+						<p class="text-gray-100 text-sm">#Lorem ipsum</p>
+						<p class="text-gray-100 text-sm">#Lorem ipsum</p>
+						<p class="text-gray-100 text-sm">#Lorem ipsum</p>
+					</div>
+				</div> -->
+			</div>
 
 			<!-- User-related -->
-			<div class="flex items-center">
-				<!-- User Avatar -->
-				<div style="width: 2rem; height: 2rem; background-image: url('https://static.wikia.nocookie.net/101dalmatianstreet/images/c/c4/Dylan.jpeg/revision/latest/scale-to-width-down/310?cb=20200208005325'); background-repeat: no-repeat; background-position: center; background-size: auto;" class="rounded-full mx-2"></div>
-
-				<!-- Control Panel -->
-				<button on:mouseover={(e) => button01Hovered = true} on:mouseout={(e) => button01Hovered = false} class="px-2 py-2 mx-2 hover:bg-white rounded-md relative">
-					<svg style="width: 1.2rem; height: 1.2rem;" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="{ button01Hovered ? "#000" : "#fff" }" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-grid"><rect x="3" y="3" width="7" height="7"></rect><rect x="14" y="3" width="7" height="7"></rect><rect x="14" y="14" width="7" height="7"></rect><rect x="3" y="14" width="7" height="7"></rect></svg>
-				
-					<span class="absolute top-0 right-0 rounded-full w-4 h-4 text-white flex items-center justify-center bg-red-400">
-						<p class="block text-extra-xss">9+</p>
-					</span>
-				</button>
-			</div>
+			<UserLine />
 		</header>
 
-		<!-- Maskot: Helper
-		<div style="z-index: 0;" class="absolute inset-x-0 top-0 w-full text-white">
-			<div>
-				#Arrow icon
-				<svg xmlns="http://www.w3.org/2000/svg" width="30" height="60"><g fill="none" stroke="#fff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10"><path d="M19.409 6.077C14.118 8.315 5.456 11.935 4.693 18.628c-.676 5.938 5.108 11.437 8.879 15.21 5.497 5.502 24.226 21.344 6.303 23.567-5.914.733-11.722-.262-17.337-2.13"/><path d="M19.307 13.743c.426-4.367 3.978-6.833 5.197-10.849-3.177.411-9.656.953-12.678-.541"/></g></svg>
+		<main style="z-index: 1;" class="bg-gray-100 w-full flex flex-col items-center justify-center">
 
-				#Text Area
-				<div class="rounded-md">
-					<p class="text-white text-sm">- Тут ты сможешь найти весь функционал этого сайта! Тоесть все странички, отдельные сервисы и другую подобную мишуру!</p>
+			<!-- Some kind of a Hero Menu -->
+			<div style="background-image: url('https://source.unsplash.com/random/1000x900'); background-size: cover; background-repeat: no-repeat;" class="w-full h-100vh relative">
+				<!-- Overlay -->
+				<div style="background: rgba(0,0,0,0.6)" class="absolute inset-0 w-full h-full flex justify-center items-center">
+					<h1 class="text-white text-2xl font-semibold">Hello there!</h1>
 				</div>
 			</div>
-		</div> -->
 
-		<main style="z-index: 1;" class="bg-gray-100 w-full h-100vh flex items-center justify-center">
-			<p class="text-black text-3xl">WIP</p>	
+			<!-- Content -->
+			<div class="w-full h-full flex flex-col md:flex-row flex-wrap justify-center px-2 py-16 pt-36">
+				{ #each posts as post }
+					<div class="w-full md:w-1/2 lg:w-1/4 px-2 py-2 relative">
+						<!-- Post Content
+						@type: Image Submission -->
+						<div style="padding-top: 100%; background-image: url('https://source.unsplash.com/random/900x900'); background-size: cover; background-repeat: no-repeat;" class="relative w-full">
+							<!-- Overlay -->
+							<div style="background: linear-gradient(180deg, rgba(255,255,255,0) 0%, rgba(0,0,0,1) 100%);" class="absolute inset-0 w-full h-full flex items-end">
+								<!-- Text and some Statistics -->
+
+								<div class="w-full py-2 px-3">
+									<!-- Submission name -->
+									<span class="flex items-center">
+										<!-- Type Icon -->
+										<svg style="width: 1.2rem; height: 1.2rem;" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-image"><rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect><circle cx="8.5" cy="8.5" r="1.5"></circle><polyline points="21 15 16 10 5 21"></polyline></svg>
+										<p class="text-white text-xl font-semibold ml-1">Some text</p>
+									</span>
+
+									<!-- Submission author -->
+									<p class="text-gray-100 text-sm">by <span class="border-b-1 border-dotted border-gray-700">Juiipup</span></p>
+								
+									<!-- Statistics -->
+									<div class="w-full flex justify-start mt-1">
+										<!-- Likes -->
+										<span class="mr-3 flex items-center">
+											<svg style="width: 0.8rem; height: 0.8rem;" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#a0aec0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-heart"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>
+											<p class="text-extra-xs text-gray-500 ml-1">212</p>
+										</span>
+
+										<!-- Comments -->
+										<span class="mr-3 flex items-center">
+											<svg style="width: 0.8rem; height: 0.8rem;" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#a0aec0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-message-circle"><path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"></path></svg>
+											<p class="text-extra-xs text-gray-500 ml-1">16</p>
+										</span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				{ /each }
+			</div>
 		</main>
 	</div>
 { /if }
