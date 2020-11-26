4e724b251b8387af5a5ff431f5892ef9fa8f1a72
SniperFox213 <dj.gleb@list.ru>
2020-11-26 17:49:17 +0200

    Landing Changes

--- src/components/prototype/Hero.svelte
+++ src/components/prototype/Hero.svelte
@@ -130,6 +130,67 @@
 							</div>
 						</div>
           </div>
+
+          <!-- 
+            Post Content
+            @type: Text
+           -->
+          <div class="w-full md:w-1/3 p-2 relative">
+						<!-- Post Content
+						@type: Image Submission -->
+						<div style="padding-top: 100%;" class="relative bg-black w-full">
+							<!-- Overlay -->
+							<div class="absolute inset-0 w-full h-full">
+								<!-- Text and some Statistics -->
+                
+                <div class="relative flex flex-col px-3 py-2 w-full h-full">
+                  <!-- Submission name -->
+                  <span class="w-full flex justify-between items-center pb-2">
+                    <div class="flex items-center">
+                      <!-- Type Icon -->
+                      <svg style="width: 1.2rem; height: 1.2rem;" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
+                      
+                      <div class="ml-1 pt-2">
+                        <p style="line-height: 0.5;" class="cursor-pointer text-white text-xl font-semibold">Some text</p>
+                        <span style="line-height: 0.5;" class="cursor-pointer text-extra-xs text-white border-b-1 border-dotted border-gray-200">Щенячий Патруль</span>
+                      </div>
+                    </div>
+
+                    <!-- "Settings Icon" -->
+                    <button class="rounded-md text-white hover:bg-white hover:text-black p-2">
+                      <svg style="width: 1rem; height: 1rem;" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
+                    </button>
+                  </span>
+
+                  <!-- Small Description -->
+                  <div style="overflow: hidden; overflow-y: scroll;" class="w-full flex-grow relative py-2">
+                    <div style="z-index: 2;" class="absolute inset-0 w-full h-full">
+                      <!-- Description -->
+                      <p class="text-gray-200 text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Est laborum delectus quia placeat, perferendis distinctio atque recusandae enim qui voluptate ducimus porro accusamus eum! Ratione iure neque aspernatur quibusdam repudiandae! Accusamus quibusdam sunt quas! Quod vero optio ducimus dicta, repellat tenetur architecto quidem debitis nobis soluta laudantium ipsam modi quia. Corporis et pariatur sunt illum consequatur illo assumenda impedit modi vitae nulla, atque nostrum a, rerum, expedita id? Quibusdam, veritatis. Ullam debitis odio, saepe est vitae assumenda ut, totam rem aut perferendis distinctio illo maxime ipsa veniam doloremque asperiores, architecto modi fugiat laboriosam expedita dolorem eveniet ducimus veritatis? Nesciunt, vel minima, ad nihil illo, optio perferendis deleniti provident cum ut aliquid assumenda distinctio nulla? Provident quo magnam expedita ullam eaque.</p>
+                    </div>
+                  </div>
+
+                  <!-- Submission author -->
+                  <p class="text-gray-100 text-sm">by <span class="border-b-1 border-dotted border-gray-700">Juiipup</span></p>
+                
+                  <!-- Statistics -->
+                  <div class="w-full flex justify-start mt-1">
+                    <!-- Likes -->
+                    <span class="mr-3 flex items-center">
+                      <svg style="width: 0.8rem; height: 0.8rem;" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#a0aec0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-heart"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>
+                      <p class="text-extra-xs text-gray-500 ml-1">212</p>
+                    </span>
+
+                    <!-- Comments -->
+                    <span class="mr-3 flex items-center">
+                      <svg style="width: 0.8rem; height: 0.8rem;" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#a0aec0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-message-circle"><path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"></path></svg>
+                      <p class="text-extra-xs text-gray-500 ml-1">16</p>
+                    </span>
+                  </div>
+                </div>
+              </div>
+						</div>
+          </div>
         </div>
       </div>
     {/if}
